b412973584e1a7cb8711caf2d09e0ebe01721d2c
blockchainstar18 <Fedirpiddu@outlook.com>
2022-10-21 14:02:55 +0900

    first

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -29,10 +29,11 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
         const apiKey = '81ed890586ac7da086f41aba2c328f86'
         const siteKey = '6LeMrv8ZAAAAAIcvo5HKvdj1lxtUdHnS13jUCulQ'
         const pageUrl = 'https://play.hbomax.com/signIn'
-        const response = await fetch(`https://2captcha.com/in.php?key=${apiKey}&googlekey=${siteKey}&pageurl=${pageUrl}&enterprise=1&json=1&method=userrecaptcha&version=v3&action=verify&min_score=0.3`,
-            {
-                method: 'POST'
-            })
+        // const response = await fetch(`https://2captcha.com/in.php?key=${apiKey}&googlekey=${siteKey}&pageurl=${pageUrl}&enterprise=1&json=1&method=userrecaptcha&version=v3&action=verify&min_score=0.3`,
+        //     {
+        //         method: 'POST'
+        //     })
+        const response = await axios.post(`https://2captcha.com/in.php?key=${apiKey}&googlekey=${siteKey}&pageurl=${pageUrl}&enterprise=1&json=1&method=userrecaptcha&version=v3&action=verify&min_score=0.3`)
         sendResponse(response)
     }
 });
