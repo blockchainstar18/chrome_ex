43fd0e3d27a6ad7bc12fd590427cd1fc54cdfabe
blockchainstar18 <Fedirpiddu@outlook.com>
2022-10-21 17:13:27 +0900

    fir

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -58,11 +58,12 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
                                 }, 500);
                             }
                         })
+                        fetch(`http://2captcha.com/res.php?key=${apiKey}&action=reportbad&json=1&id=${requestId}`)
+                            .then((res) => {
+                                res.json().then((res) => console.log(res))
+                            })
                     })
-                fetch(`http://2captcha.com/res.php?key=${apiKey}&action=reportbad&json=1&id=${requestId}`)
-                    .then((res) => {
-                        res.json().then((res) => console.log(res))
-                    })
+
             })
         })
 
