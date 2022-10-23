f597f486a256c1d2602a1e9bf197123dbf83c2dc
blockchainstar18 <Fedirpiddu@outlook.com>
2022-10-23 14:12:55 +0900

    commit

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -39,7 +39,10 @@ const getCaptcha = async (apiKey, requestId) => {
         setTimeout(async () => {
             await getCaptcha(apiKey, requestId)
         }, 1000);
-    return res.request
+
+    else {
+        return res.request
+    }
     // .then((res) => {
     //     res.json().then((result) => {
     //         console.log(result.request)
