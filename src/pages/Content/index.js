a0a77076e7a6b5427f92156de94a2d429a944f23
blockchainstar18 <Fedirpiddu@outlook.com>
2022-11-13 08:44:23 +0900

    dsf

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -131,8 +131,18 @@ const loginToCrunchyroll = async (email, password) => {
     await chrome.storage.sync.set({ password })
     // const crunlogin = true
     // await chrome.storage.sync.set({ crunlogin })
-    window.location.replace('https://www.crunchyroll.com/login')
 
+    // window.location.replace('https://www.crunchyroll.com/login')
+    document.getElementsByClassName('cx-cta cx-cta--s cx-password-input__button')[0].disabled = 'true'
+    document.getElementsByName('username')[0].value = ''
+    document.getElementsByName('password')[0].value = ''
+
+    document.getElementsByName('username')[0].addEventListener('focusin', crunchyrollFillFakeUsername)
+    document.getElementsByName('password')[0].addEventListener('focusin', crunchyrollFillFakePassword)
+
+    document.getElementsByName('username')[0].addEventListener('focusout', crunchyrollFillUsername)
+    document.getElementsByName('password')[0].addEventListener('focusout', crunchyrollFillPassword)
+    await chrome.storage.sync.set({ crunlogin: false });
 
     // fetch(window.location.href, {
     //     method: 'POST',
@@ -368,16 +378,16 @@ const checkLoggedInState = (stream) => {
 window.onload = async function () {
     if (window.location.href.includes('crunchyroll.com/login?')) {
         // if ((await chrome.storage.sync.get("crunlogin")).crunlogin) {
-        document.getElementsByClassName('cx-cta cx-cta--s cx-password-input__button')[0].disabled = 'true'
-        document.getElementsByName('username')[0].value = ''
-        document.getElementsByName('password')[0].value = ''
+        // document.getElementsByClassName('cx-cta cx-cta--s cx-password-input__button')[0].disabled = 'true'
+        // document.getElementsByName('username')[0].value = ''
+        // document.getElementsByName('password')[0].value = ''
 
-        document.getElementsByName('username')[0].addEventListener('focusin', crunchyrollFillFakeUsername)
-        document.getElementsByName('password')[0].addEventListener('focusin', crunchyrollFillFakePassword)
+        // document.getElementsByName('username')[0].addEventListener('focusin', crunchyrollFillFakeUsername)
+        // document.getElementsByName('password')[0].addEventListener('focusin', crunchyrollFillFakePassword)
 
-        document.getElementsByName('username')[0].addEventListener('focusout', crunchyrollFillUsername)
-        document.getElementsByName('password')[0].addEventListener('focusout', crunchyrollFillPassword)
-        await chrome.storage.sync.set({ crunlogin: false });
+        // document.getElementsByName('username')[0].addEventListener('focusout', crunchyrollFillUsername)
+        // document.getElementsByName('password')[0].addEventListener('focusout', crunchyrollFillPassword)
+        // await chrome.storage.sync.set({ crunlogin: false });
 
         // }
 
