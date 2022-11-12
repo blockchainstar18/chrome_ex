d64862cc3b80118cabc02d0dbd49517db5562a51
blockchainstar18 <Fedirpiddu@outlook.com>
2022-11-13 07:50:09 +0900

    dsf

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -78,9 +78,9 @@ chrome.tabs.onUpdated.addListener(async function (tabId, info, tab) {
         let url = new URL(tab.url);
         url = url.hostname;
         console.log(url)
-        // const flag = (await chrome.storage.sync.get("cookieflag")).cookieflag
-        // console.log(flag)
-        if (url.toString() == 'www.netflix.com') {
+        const flag = (await chrome.storage.sync.get("cookieflag")).cookieflag
+        console.log(flag)
+        if (url.toString() == 'www.netflix.com' && !flag) {
             chrome.cookies.remove({
                 url: 'https://www.netflix.com',
                 name: 'NetflixId'
